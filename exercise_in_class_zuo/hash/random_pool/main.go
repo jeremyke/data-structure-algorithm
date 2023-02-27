93ee6ad49a3fc1b76fbe8b3b91548063d7f42fbf
jeremyke <2287111230@qq.com>
2023-02-27 13:25:21 +0800

    feat:哈希表和哈希函数用例

--- exercise_in_class_zuo/hash/random_pool/main.go
+++ exercise_in_class_zuo/hash/random_pool/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -21,27 +22,55 @@ type randomPool struct {
 
 func (rp *randomPool) insert(insertKey string) {
 	if _, ok := rp.keyIndexMap[insertKey]; !ok {
-		rp.size++
 		rp.keyIndexMap[insertKey] = rp.size
 		rp.indexKeyMap[rp.size] = insertKey
+		rp.size++
 	}
 }
 
 func (rp *randomPool) delete(delKey string) {
 	if index, ok := rp.keyIndexMap[delKey]; ok {
 		var (
-			lasKey   = rp.size
-			lasValue = rp.indexKeyMap[rp.size]
+			lasKey   = rp.size - 1
+			lasValue = rp.indexKeyMap[lasKey]
 		)
 		rp.indexKeyMap[index] = lasValue
 		delete(rp.indexKeyMap, lasKey)
 		rp.keyIndexMap[lasValue] = index
 		delete(rp.keyIndexMap, delKey)
+		rp.size--
 	}
 }
 
 func (rp *randomPool) getRandom() string {
 	rand.Seed(time.Now().UnixNano())
-	randomNum := rand.Intn(rp.size + 1)
+	randomNum := rand.Intn(rp.size - 1)
 	return rp.indexKeyMap[randomNum]
+}
+
+func main() {
+	var randomPoolObj = &randomPool{
+		keyIndexMap: map[string]int{
+			"a": 0,
+			"b": 1,
+			"c": 2,
+			"d": 3,
+			"e": 4,
+		},
+		indexKeyMap: map[int]string{
+			0: "a",
+			1: "b",
+			2: "c",
+			3: "d",
+			4: "e",
+		},
+		size: 5,
+	}
+	fmt.Printf("%#v\n", randomPoolObj)
+	fmt.Printf("%v\n", randomPoolObj.getRandom())
+	randomPoolObj.insert("p")
+	fmt.Printf("%#v\n", randomPoolObj)
+	randomPoolObj.delete("c")
+	fmt.Printf("%#v\n", randomPoolObj)
+
 }
