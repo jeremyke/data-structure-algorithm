462fa82a08ab0725128e7764e6565e2990836274
jeremyke <2287111230@qq.com>
2023-03-02 00:45:04 +0800

    feat:二分查找leetcode算法题

--- leetcode/81-search-in-rotated-sorted-array-ii/main.go
+++ leetcode/81-search-in-rotated-sorted-array-ii/main.go
@@ -1,5 +1,50 @@
 package main
 
-func search(nums []int, target int) bool {
+import "fmt"
 
+/**
+已知存在一个按非降序排列的整数数组 nums ，数组中的值不必互不相同。
+
+在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为 [4,5,6,6,7,0,1,2,4,4] 。
+
+给你 旋转后 的数组 nums 和一个整数 target ，请你编写一个函数来判断给定的目标值是否存在于数组中。如果 nums 中存在这个目标值 target ，则返回 true ，否则返回 false 。
+
+你必须尽可能减少整个操作步骤。
+
+
+*/
+
+func search(nums []int, target int) bool {
+	if len(nums) == 1 {
+		return nums[0] == target
+	}
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if target == nums[mid] {
+			return true
+		}
+		if nums[l] == nums[mid] && nums[mid] == nums[r] {
+			l++
+			r--
+		} else if nums[l] <= nums[mid] { //顺序
+			if target >= nums[l] && target < nums[mid] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else { //非顺序
+			if target > nums[mid] && target <= nums[len(nums)-1] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+	return false
+}
+
+func main() {
+	var arr = []int{1, 0, 1, 1, 1}
+	fmt.Println(search(arr, 0))
 }
