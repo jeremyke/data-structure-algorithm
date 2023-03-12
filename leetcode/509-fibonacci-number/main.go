e69e194de9ff3bc08d4c467fe2f3117aff741019
jeremyke <2287111230@qq.com>
2023-03-12 17:19:25 +0800

    feat:斐波拉契函数

---
+++ leetcode/509-fibonacci-number/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+/**
+斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+
+F(0) = 0，F(1) = 1
+F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+给定 n ，请计算 F(n) 。
+*/
+
+func fib(n int) int {
+	//base case
+	if n == 0 || n == 1 {
+		return n
+	}
+	//穷举
+	prev, curr := 0, 1
+	for i := 2; i <= n; i++ {
+		sum := prev + curr
+		prev = curr
+		curr = sum
+	}
+	return curr
+}
+
+func main() {
+	fmt.Println(fib(4))
+}
